0eec084aef931f2fb50e12fe6763a5038454718e
wykys <vvykys@gmail.com>
2018-06-04 19:27:52 +0200

    updated hm-trp

--- main/Inc/hm-trp.hpp
+++ main/Inc/hm-trp.hpp
@@ -1,20 +1,26 @@
-#ifndef __HMTRP_INCLUDED__
-#define __HMTRP_INCLUDED__
+#ifndef __HMTRP_INLUDED__
+#define __HMTRP_INLUDED__
 
 #include "stm32f4xx_hal.h"
+#include "cm-uart.hpp"
+#include "cm-macro.hpp"
 #include <vector>
 #include <string>
 
 using namespace std;
 
-class HMTRP
+class HMTRP : public UART
 {
 private:
     void enable(bool ena = true);
     void config(bool cnf = true);
+    void cmd(vector<uint8_t> vector_bytes);
+    void reset_to_default(void);
+    bool check_data_rate(uint32_t data_rate);
+    void set_wireless_data_rate(uint32_t baudrate);
+    void set_uart_transfer_speed(uint32_t baudrate);
 public:
-    HMTRP();
-    void send(string str);
+    HMTRP(UART_HandleTypeDef *huart, uint32_t baudrate = 9600);
 };
 
-#endif // ifndef __HMTRP_INCLUDED__
+#endif // ifndef __HMTRP_INLUDED__
--- main/Src/hm-trp.cpp
+++ main/Src/hm-trp.cpp
@@ -1,11 +1,21 @@
 #include "hm-trp.hpp"
 
-extern UART_HandleTypeDef huart2;
-
-HMTRP::HMTRP()
+HMTRP::HMTRP(UART_HandleTypeDef *huart, uint32_t baudrate) : UART(huart)
 {
     config(false);
     enable(true);
+
+    /*for (uint32_t bd : {1200, 1800, 2400, 4800, 9600, 19200, 38400, 57600, 115200})
+     * {
+     *  set_baudrate(bd);
+     *  reset_to_default();
+     *  HAL_Delay(10);
+     * }*/
+
+    /*set_baudrate(9600);
+     * set_wireless_data_rate(baudrate);
+     * set_uart_transfer_speed(baudrate);*/
+    rx_it();
 }
 
 void HMTRP::enable(bool ena)
@@ -18,7 +28,56 @@ void HMTRP::config(bool cnf)
     HAL_GPIO_WritePin(RF_CONFIG_GPIO_Port, RF_CONFIG_Pin, (cnf) ? GPIO_PIN_RESET : GPIO_PIN_SET);
 }
 
-void HMTRP::send(string str)
+void HMTRP::cmd(vector<uint8_t> vector_bytes)
 {
-    HAL_UART_Transmit(&huart2, (uint8_t *) str.c_str(), str.size(), 0xFFFFFF);
+    config(true);
+    tx(vector_uint8_t_array(0xAA, 0xFA));
+    tx(vector_bytes);
+    config(false);
+    HAL_Delay(10);
+}
+
+void HMTRP::reset_to_default(void)
+{
+    vector<uint8_t> conf;
+    conf.push_back(0xF0);
+    cmd(conf);
+}
+
+bool HMTRP::check_data_rate(uint32_t data_rate)
+{
+    for (uint32_t dr : { 1200, 1800, 2400, 4800, 9600, 19200, 38400, 57600, 115200 })
+        if (data_rate == dr)
+            return true;
+
+    return false;
+}
+
+void HMTRP::set_wireless_data_rate(uint32_t baudrate)
+{
+    if (check_data_rate(baudrate))
+    {
+        vector<uint8_t> conf;
+        conf.push_back(0xC3);
+        conf.push_back(uint8_t((baudrate >> 24) & 0xFF));
+        conf.push_back(uint8_t((baudrate >> 16) & 0xFF));
+        conf.push_back(uint8_t((baudrate >> 8) & 0xFF));
+        conf.push_back(uint8_t(baudrate & 0xFF));
+        cmd(conf);
+    }
+}
+
+void HMTRP::set_uart_transfer_speed(uint32_t baudrate)
+{
+    if (check_data_rate(baudrate))
+    {
+        vector<uint8_t> conf;
+        conf.push_back(0xE1);
+        conf.push_back(uint8_t((baudrate >> 24) & 0xFF));
+        conf.push_back(uint8_t((baudrate >> 16) & 0xFF));
+        conf.push_back(uint8_t((baudrate >> 8) & 0xFF));
+        conf.push_back(uint8_t(baudrate & 0xFF));
+        cmd(conf);
+        set_baudrate(baudrate);
+    }
 }
