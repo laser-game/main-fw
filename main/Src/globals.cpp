7336d9301fa5db5cc85b1b0b198fed7d924e7638
wykys <vvykys@gmail.com>
2018-06-06 00:56:20 +0200

    added gebug callback to globals

--- main/Src/globals.cpp
+++ main/Src/globals.cpp
@@ -7,8 +7,11 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
     if (huart->Instance == global->hmtrp->get_huart()->Instance)
     {
         global->radio_buffer_rx->insert(global->hmtrp->buffer_rx[0]);
-
         global->hmtrp->rx_it();
+    }
+    else if (huart->Instance == global->debug->get_huart()->Instance)
+    {
+        
     }
 }
 
